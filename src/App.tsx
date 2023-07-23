18ef64790c416081dd84fe73a87948ab6d147bcd
xninjaxbr <81336328+xninjaxbr@users.noreply.github.com>
2023-07-23 16:02:37 -0300

    criação de trasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import './App.css'
+import { Header } from './Components/Header'
+import { NewTask } from './Components/NewTask'
+import { TasksContainer } from './Components/TasksContainer'
+import './globals.css'
+import { useState } from 'react'
 
-export function App() {
-  
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+export interface Itask{
+    
+  id: number,
+  status: boolean,
+  task: string,
 }
 
+  export function App() {
+    
+  const [tasks, setTask] = useState<Itask[]>([])
 
+  const hadtask = tasks.length == 0  
+
+  function handleNewTask(newtask : Itask) {
+    
+    console.log(newtask)
+    setTask( [...tasks, newtask])
+    console.log(newtask)
+      
+  }
+
+  return (
+   <div >
+    <Header />
+    <NewTask handle={handleNewTask} />
+    <TasksContainer hadtask={hadtask} tasks={tasks}/>
+   </div>  
+  )
+}
